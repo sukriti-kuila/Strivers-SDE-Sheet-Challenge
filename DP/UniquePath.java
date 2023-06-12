2afa59fe74dbf86e4fc6a5c0e41e3da120f8119b
Sukriti Kuila <87015685+sukriti-kuila@users.noreply.github.com>
2023-06-12 23:25:55 +0530

    Update UniquePath.java

--- DP/UniquePath.java
+++ DP/UniquePath.java
@@ -1,5 +1,6 @@
 import java.util.* ;
 import java.io.*; 
+// Memoization
 public class Solution {
 	public static int uniquePaths(int m, int n) {
 		int [][] dp = new int [m][n];
@@ -16,4 +17,33 @@ public class Solution {
         int left = memoization(r,c-1,dp);
         return dp[r][c] = up+left;
     }
+}
+
+// tabulation
+public static int uniquePaths(int m, int n) {
+		return tabulation(m,n);
+	}
+	private static int tabulation (int m, int n) {
+		int [][] dp = new int [m][n];
+		for (int i=0; i<m; i++)
+		{
+			for (int j=0; j<n; j++)
+			{
+				if (i==0&&j==0)
+				{
+					dp[0][0] = 1;
+					continue;
+				}
+				int up = 0;
+				int left = 0;
+				if (i>0)
+					up = dp[i-1][j];
+				if (j>0)
+					left = dp[i][j-1];
+				dp[i][j] = left+up;		
+			}
+		}
+		return dp[m-1][n-1];
+	}
+
 }
