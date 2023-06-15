66a48b50162fc3a70a3991d7a4685457fc682363
Sukriti Kuila <87015685+sukriti-kuila@users.noreply.github.com>
2023-06-15 20:29:05 +0530

    Update Longest Substring Without Repeating Characters.java

--- Arrays/Longest Substring Without Repeating Characters.java
+++ Arrays/Longest Substring Without Repeating Characters.java
@@ -1,3 +1,33 @@
+// Brute
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        ArrayList <Character> list = new ArrayList();
+        int l = 0, r=0;
+        int len = 0;
+        while (r<s.length())
+        {
+            char ch = s.charAt(r);
+            if (!list.contains(ch))
+            {
+                list.add(ch);
+                len = Math.max(len, (r-l)+1);
+                r++;
+            }
+            else
+            {
+                while (list.contains(ch))
+                {
+                    list.remove(0);
+                    l++;
+                }
+            }
+        }
+        return len;
+    }
+}
+
+// optimal
+
 import java.util.* ;
 import java.io.*; 
 public class Solution 
